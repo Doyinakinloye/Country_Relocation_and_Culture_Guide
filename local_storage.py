e961cf7cf1a2327666b513209d02eb2ada567bff
Akinloye Oluwadoyinsolami <doyinakinloye07@gmail.com>
2026-06-20 11:56:15 +0100

    Update local_storage.py

--- local_storage.py
+++ local_storage.py
@@ -5,10 +5,12 @@ import os
 class LocalStorage:
     def __init__(self, filename="data.json"):
         self.filename = filename
+        # if file does not exit, create it
         if not os.path.exists(self.filename):
             with open(self.filename, "w") as file:
                 json.dump({}, file)
 
+    # save the data with the key
     def save(self, key, data):
         try:
             with open(self.filename, "r") as file:
@@ -19,6 +21,7 @@ class LocalStorage:
         except Exception:
             raise IOError("Error saving data.")
 
+    # load a specific data with the key
     def load(self, key):
         try:
             with open(self.filename, "r") as file:
@@ -27,6 +30,7 @@ class LocalStorage:
         except Exception:
             raise IOError("Error loading data.")
 
+    # load all the data in the json file
     def load_all(self):
         try:
             with open(self.filename, "r") as file:
